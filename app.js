754a863202c26b99508615691d46fc12c43ef0f9
asinghal <asinghal@tekion.com>
2018-12-25 17:39:16 +0530

    App.js Change

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 const express = require('express');
-const http = require('http');
 const path = require('path');
-let app = express();
+const app = express();
+
 app.use(express.static(path.join(__dirname, 'build')));
-const port = process.env.PORT || '8080';
-app.set('port', port);
-const server = http.createServer(app);
-server.listen(port, () => console.log(`Running on localhost:${port}`));
+
+app.get('/', function(req, res) {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
+
+app.listen(9000);
