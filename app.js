152ad18fe61a079c64a2b5a0f63e324944b63063
asinghal <asinghal@tekion.com>
2018-12-25 17:48:53 +0530

    App

--- app.js
+++ app.js
@@ -8,4 +8,6 @@ app.get('/*', function (req, res) {
     res.sendFile(path.join(__dirname, 'build', 'index.html'));
   });
 
-app.listen(9000);
+  app.listen(process.env.PORT || 3000, function(){
+    console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+  });
